bf36afb6ac3643926f0d55b3b605f8da9a1d5f50
Nickolay <13knive@gmail.com@>
2024-03-18 14:37:29 +0500

    frst

--- ConsoleApplication24/ConsoleApplication24.cpp
+++ ConsoleApplication24/ConsoleApplication24.cpp
@@ -22,7 +22,11 @@ void temp_dir()
 	fs::path tmp_dir = fs::temp_directory_path();
 
 }
-string abs_path(string name)
+void abs_path(string name)
+{
+	cout<<fs::absolute(name).string();
+}
+string abs_pathr(string name)
 {
 	return fs::absolute(name).string();
 }
@@ -50,17 +54,17 @@ void dir_explorer(string path)
 		if (entry.is_directory())
 		{
 			cout << "Directory\t" + entry.path().filename().string()+"\t"+ to_string(fs::file_size(entry.path().filename().string()))+" Byte" << endl;
-			dir_log("Directory\t" + entry.path().filename().string() + "\t" + to_string(fs::file_size(entry.path().filename().string())) + " Byte");
+			//dir_log("Directory\t" + entry.path().filename().string() + "\t" + to_string(fs::file_size(entry.path().filename().string())) + " Byte");
 			
 		}
 		else
 		{
 			cout << "File\t" + entry.path().filename().string() + "\t" + to_string(fs::file_size(entry.path().filename().string())) + " Byte" << endl;
-			dir_log("File\t" + entry.path().filename().string() + "\t" + to_string(fs::file_size(entry.path().filename().string())) + " Byte");
+			//dir_log("File\t" + entry.path().filename().string() + "\t" + to_string(fs::file_size(entry.path().filename().string())) + " Byte");
 		}
-		string filenm =abs_path(entry.path().filename().string())+ entry.path().filename().string();
-		if (filenm[filenm.size() - 1] == 't')
-			copy_object(entry.path().filename().string(), filenm);
+		//string filenm =abs_path(entry.path().filename().string())+ entry.path().filename().string();
+		//if (filenm[filenm.size() - 1] == 't')
+			//copy_object(entry.path().filename().string(), filenm);
 
 	}
 }
@@ -91,10 +95,46 @@ void dir_delete(string name)
 
 int main()
 {
+	
 	string path = ".";
-	string name = "test";
-	dir_explorer(path);
-	// создать папку и скопировать в нее все txt файлы из корневой папки
+	while (true) {
+		string dest = abs_pathr(path);
+		cout << dest << endl;
+		cout << "\nShow directory, input: s" << endl;
+		cout << "Copy file, input: c" << endl;
+		cout << "Goto, input: g" << endl;
+		cout << "Delete file, input: d" << endl;
+		cout << "Create file (directory), input: n" << endl;
+		cout << "Abs_link file, input: a" << endl;
+		string key;
+		cin >> key;
+		if (key == "c")
+		{
+			cout << "Copy file name:\n";
+			string fl_name;
+			string new_name;
+			cin >> fl_name;
+			cout << "Copy new_file name:\n";
+			cin >> new_name;
+			copy_object(fl_name, new_name);
+		}
+		if (key == "a")
+		{
+			cout << "File name:\n";
+			string fl_name;
+			cin >> fl_name;
+			abs_path(fl_name);
+		}
+		if (key == "r")
+		{
+
+			string new_dir;
+			cin >> new_dir;
+			path = new_dir;
+		}
+	}
+
+
 	
 	
 }
